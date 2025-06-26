ab5d2e8a15c1e7bedc05b0d78ebc3af9feca2f83
apolignano <alessandro.polignano@ebi.ac.uk>
2025-06-26 12:04:45 +0100

    Fix redirection

--- src/index.js
+++ src/index.js
@@ -1,65 +1,32 @@
 import { interproURL, test, basepath } from "../config.json";
 import getNewURL from "./get-new-url";
 
-const url = new URL(document.location.href);
-let pathname = url.pathname;
-if (url.pathname.startsWith(basepath)) {
-  pathname = url.pathname.slice(basepath.length);
-} else if (basepath === "?") {
-  pathname = url.search.slice(2);
-} else {
-  console.error("the pathname doesn't start with the given base path");
-}
-const urlParts = pathname.split("/");
-
-let newURL = getNewURL(urlParts);
-
-if (newURL) {
+(async () => {
   document.querySelector(".redirect").classList.add("show");
+  const url = new URL(document.location.href);
+  let pathname = url.pathname;
 
-  let seqLeft = 5;
-  let playing = true;
-
-  const counterID = setInterval(() => {
-    if (!playing) return;
-    seqLeft = Math.max(seqLeft - 1, 0);
-    document
-      .querySelectorAll(".sec-left")
-      .forEach((element) => (element.innerHTML = seqLeft));
-    if (seqLeft === 0) {
-      if (test) console.log(newURL);
-      else window.location.replace(newURL);
-      clearInterval(counterID);
-    }
-  }, 1000);
-
-  const setNewURL = (theNewURL) => {
-    if (theNewURL === null) {
-      theNewURL = urlParts?.[1]
-        ? `${interproURL}/search/text/${urlParts[1]}`
-        : interproURL;
-    }
-    const iproLink = document.getElementById("linkToInterPro");
-    iproLink.setAttribute("href", theNewURL);
-    iproLink.innerHTML = theNewURL;
-    newURL = theNewURL;
-  };
-
-  const svg = document.getElementById("countdown");
-  svg.addEventListener("click", () => {
-    playing = !playing;
-    if (playing) {
-      svg.classList.remove("paused");
-    } else {
-      svg.classList.add("paused");
-    }
-  });
-
-  if (typeof newURL?.then === "function") {
-    newURL.then((theNewURL) => setNewURL(theNewURL));
+  if (url.pathname.startsWith(basepath)) {
+    pathname = url.pathname.slice(basepath.length);
+  } else if (basepath === "?") {
+    pathname = url.search.slice(2);
   } else {
-    setNewURL(newURL);
+    console.error("The pathname doesn't start with the given base path");
   }
-} else {
-  document.querySelector(".fake-home").classList.add("show");
-}
+
+  const urlParts = pathname.split("/");
+
+  let resolvedURL = await getNewURL(urlParts);
+
+  if (!resolvedURL) {
+    resolvedURL = urlParts?.[1]
+      ? `${interproURL}/search/text/${urlParts[1]}`
+      : interproURL;
+  }
+
+  if (test) {
+    console.log(resolvedURL);
+  } else {
+    window.location.replace(resolvedURL);
+  }
+})();
